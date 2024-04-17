8b22cc6f056bc6ea313d8812912bf5fbf59f60e5
deepak-rk <deepakradhakrishnan.m@gmail.com>
2024-04-17 18:00:00 +0400

    updated config retry attempts

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,8 @@ module.exports = defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  //retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -29,8 +30,9 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+   
   },
-
+ 
   /* Configure projects for major browsers */
   projects: [
     {
